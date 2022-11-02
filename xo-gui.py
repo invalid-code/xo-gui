1ad0cd99cac06fde1879da68d1055cc4ddfebab6
invalid-jess <jessgaming55@gmail.com>
2022-11-02 13:15:36 +0800

    polished my code

--- xo-gui.py
+++ xo-gui.py
@@ -6,42 +6,54 @@ import PySimpleGUI as sg
 
 sg.theme("dark grey")
 
-
 class Player:
-    def __init__(self, is_first):
-        self.is_first = True if is_first else False
-        self.player = rd.choice(Game.xo)
+    def __init__(self, player_avatar:str, is_player_opponent:str):
+        self.player = player_avatar
+        self.is_player_opponent = is_player_opponent
 
     def play(self, ind: str, game):
-
         if game.check_cell(ind):
             game.table[ind] = self.player
             return True
+        return False
+
+    
+    def __repr__(self):
+        return f"Player(player={self.player}, is_player_opponent={self.is_player_opponent})"
+
+    def __str__(self):
+        return f"player is {self.player}\nplayer is the {self.is_player_opponent}"
 
 
-class Opponent:
-    def __init__(self, player, is_first):
-        self.is_first = True if not is_first else False
-        self.opponent = "X" if player.player == "O" else "O"
+class PlayerA(Player):
+    pass
+    
 
+
+class PlayerB(Player):
     def play(self, game):
-        cell = ""
-        opponent_status = {"cell": ""}
-
-        # ai
-        # 
-
-        if game.check_cell(cell):
-            game.table[cell] = self.opponent
-            opponent_status.update({"cell": cell, "has_played": True})
-        
-        return opponent_status
+        # temporary
+        ind = ""
+        for key, cell in game.table.items():
+            if not cell:
+                game.table[key] = self.player
+                return True
+        # if game.check_cell(ind):
+        #     game.table[ind] = self.player
+        #     return True
+        # return False
 
 
 class Game:
+    # BUG fix not being able play when player is second
     xo = ("X", "O")
 
-    def __init__(self):
+    def __init__(self, player=PlayerA, opponent=PlayerB):
+        self.player_avatar = rd.choice(Game.xo)
+        self.player = player(self.player_avatar, "player")
+        self.opponent = opponent("X", "opponent")
+        if self.player_avatar == "X":
+            self.opponent = opponent("O", "opponent")
         self.first = rd.choice(Game.xo)
         self.table = {
             "1": "",
@@ -55,16 +67,16 @@ class Game:
             "9": "",
         }
     
-    def get_cell_value(self, ind:str):
+    def get_cell_value(self, ind:str): 
         return self.table.get(ind)
 
     def is_player_first(self):
-        if self.first == Game.xo[0]:
+        if self.first == self.player.player:
             return True
         return False
 
     def finished_game(self):
-        """To check if game is finished"""
+        """To check if all cells are filled"""
 
         for cell in self.table.values():
 
@@ -81,11 +93,14 @@ class Game:
 
         return False
 
-    def check_winner(self, player: Player, opponent: Opponent):
-        """To check if someone has alredy won"""
+    def check_winner(self):
+        
+        # TODO check if opponent won
+        player = self.player
+        opponent = self.opponent
         status = {"is_winner": ""}
 
-        def check_one(status=status, player=player, opponent=opponent):
+        def check_one(status:dict[str,str]=status, player=player, opponent=opponent):
             one = 1
 
             if (
@@ -106,7 +121,7 @@ class Game:
 
             return status
 
-        def check_nine(status=status, player=player, opponent=opponent):
+        def check_nine(status:dict[str,str]=status, player=player, opponent=opponent):
             nine = 9
 
             if (
@@ -127,7 +142,7 @@ class Game:
 
             return status
 
-        def check_five(status=status, player=player, opponent=opponent):
+        def check_five(status:dict[str,str]=status, player=player, opponent=opponent):
             pass
 
         one = check_one()
@@ -144,15 +159,23 @@ class Game:
 
         return status
 
+    def __repr__(self):
+        return f"Game(player_avatar={self.player_avatar}, player={self.player}, opponent={self.opponent}, first={self.first}, table={self.table})"
+
+    def __str__(self):
+        return f"the player's avatar will be {self.player_avatar}\nplayer will be {self.player}\nopponent will be {self.opponent}\n {self.player if self.first else self.opponent}\n {self.table} will be playing field"
 
 def main():
     game = Game()
-    is_first = game.is_player_first()
-    player = Player(is_first)
-    opponent = Opponent(player, is_first)
+    # is_first = game.is_player_first()
+    # player = Player(is_first)
+    # opponent = Opponent(player, is_first)
+    print(game.__repr__())
+    print(game.__str__())
 
     layout = [
-        [sg.Text(f"You'll be {player.player}.")],
+        [sg.Text(f"You'll be {game.player.player}.")],
+        [sg.Text(f"You'll be {'first' if game.is_player_first() else 'second'}.")],
         [
             sg.Column(
                 [
@@ -260,57 +283,58 @@ def main():
         if event == sg.WIN_CLOSED:
             break
 
-        if event == "-RETRY-":
+        # if event == "-RETRY-":
             game = Game()
-            # window.refresh
+            window.refresh
 
-        if not game.check_winner(player, opponent).get("is_winner"):
+        if not game_winner().get("is_winner"):
             if game.finished_game():
-                if player.is_first:
+                if game.is_player_first():
                     if event == "-CELL1-":
-                        if player.play("1", game):
-                            window[event].update(player.player)
+                        if game.player.play("1", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL2-":
-                        if player.play("2", game):
-                            window[event].update(player.player)
+                        if game.player.play("2", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL3-":
-                        if player.play("3", game):
-                            window[event].update(player.player)
+                        if game.player.play("3", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL4-":
-                        if player.play("4", game):
-                            window[event].update(player.player)
+                        if game.player.play("4", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL5-":
-                        if player.play("5", game):
-                            window[event].update(player.player)
+                        if game.player.play("5", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL6-":
-                        if player.play("6", game):
-                            window[event].update(player.player)
+                        if game.player.play("6", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL7-":
-                        if player.play("7", game):
-                            window[event].update(player.player)
+                        if game.player.play("7", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL8-":
-                        if player.play("8", game):
-                            window[event].update(player.player)
+                        if game.player.play("8", game):
+                            window[event].update(game.player.player)
                     if event == "-CELL9-":
-                        if player.play("9", game):
-                            window[event].update(player.player)
+                        if game.player.play("9", game):
+                            window[event].update(game.player.player)
 
-                    play_opponent = opponent.play(game)
-                    if play_opponent.get("has_played"):
-                        window[play_opponent.get("cell")].update(opponent.opponent)
+                    # play_opponent = opponent.play(game)
+                    # print(play_opponent)
+                    # if play_opponent.get("has_played"):
+                    #     window[play_opponent.get("cell")].update(opponent.opponent)
 
-                if opponent.is_first:
-                    pass
+                # if opponent.is_first:
+                #     pass
 
-        if game_winner(player, opponent).get("is_winner") == "win":
+        if game_winner().get("is_winner") == "win":
             window["-POPUP-"].update("You win")
             window["-RETRY-"].update(visible=True)
 
-        if game_winner(player, opponent).get("is_winner") == "lose":
+        if game_winner().get("is_winner") == "lose":
             window["-POPUP-"].update("You lose")
             window["-RETRY-"].update(visible=True)
 
-        if game_winner(player, opponent).get("is_winner") == "tie":
+        if game_winner().get("is_winner") == "tie":
             window["-POPUP-"].update("Tie")
             window["-RETRY-"].update(visible=True)
 
